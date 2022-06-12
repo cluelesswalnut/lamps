567b63ae59cf6ab8a1a20890265dec2acd63515c
Markus <ernst.sukram@gmail.com>
2022-06-12 18:10:45 -0400

    Add comments to lamps_client.py

--- lamps_client.py
+++ lamps_client.py
@@ -8,63 +8,75 @@ import requests
 import time
 from threading import Thread
 
-# import logging
-# logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 BASE_URL = "https://lamp-lamp.herokuapp.com"
 POLLING_DELAY = 1
-id = 'amanda'
+# Update the ID depending on which client the code is being deployed on
+ID = 'amanda'
 # id = 'markus'
 LAMP_ON_KEY = "lamp_on"
 COLOR_KEY = 'color'
 
 def check_server_lamp(request_session) -> bool:
+    """
+    Gets the status of the lamp from the server
+
+    @param  request_session     Session to persist the connection between requests
+    """
     api_url = BASE_URL + "/lamp"
     print(api_url)
-    param = {'id': id}
+    param = {'id': ID}
     response = request_session.get(api_url, params = param, timeout = 5)
-    # print(response.json())
-    # print(response.status_code)
     lamp_status = response.json()
-    temp = response.content
     return lamp_status
 
 def send_lamp_off():
+    """
+    Sets the lamp status on the server to off
+    """
     api_url = BASE_URL + "/lamp/off"
     response = requests.post(api_url, timeout = 5)
     response.close()
-    # print(response.json())
-    # print(response.status_code)
 
 def send_lamp_on():
+    """
+    Sets the lamp status on the server to on
+    """
     api_url = BASE_URL + "/lamp/on"
     response = requests.post(api_url, timeout = 5)
     response.close()
-    # print(response.json())
-    # print(response.status_code)
 
 def turn_on_lamp(pixels, color):
+    """
+    Turns the physical lamp on on the client side
+    """
     pixels.fill(color)
-    # pixels.fill((255,255,255))
     pixels.show()
 
 def turn_off_lamp(pixels):
+    """
+    Turns the physical lamp off on the client side
+    """
     pixels.fill((0,0,0))
     pixels.show()
 
-class Lamp:
-    # if the lamp is currently on
+class FriendshipLamp:
+    """
+    Manages a friendship lamp and keeps it synced with the server.
+    """
     _lamp_on = False
-    # sync with server if the time is later this time
-    _check_server_after = datetime.datetime.now()
-    # tells us if we are currently checking the server
-    _checking_server = False
 
+    # set the LEDs used in the lamp
     _pixels = neopixel.NeoPixel(board.D18, 8, brightness = 0.15)
 
+    # setup the GPIO pins
     GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def __enter__(self):
+        """
+        Setup a thread when a context manager is used with this class. The thread will
+        continuously check for updates from the server.
+        """
         __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(__location__, 'config.json'), 'r') as f:
@@ -77,10 +89,19 @@ class Lamp:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # self._server_thread.join()
+        """
+        Join the thread when the context manager exits
+        """
+        # TODO: does this work how I think it does?
+        self._server_thread.join()
         pass
 
     def set_color(self, hex):
+        """
+        Set the color of the lamp
+
+        @param  hex     hex code for the color
+        """
         c = hex.strip('#')
         newColor = tuple(int(c[i:i+2], 16) for i in (0, 2, 4))
         if newColor != self._color:
@@ -91,14 +112,16 @@ class Lamp:
             
 
     def check_server(self):
-        with requests.Session() as s:
+        """
+        Continuously check the server for any updates to the status of the lamp that need to be
+        applied to the physical lamp on the client side
+        """
+        # use a requests session to persist the connection instead of creating
+        # a new one for every request
+        with requests.Session() as session:
             while True:
                 try:
-                    # print('check server')
-                    # if datetime.datetime.now() < self._check_server_after:
-                    #     time.sleep(POLLING_DELAY)
-                    #     continue
-                    server_lamp_status = check_server_lamp(s)
+                    server_lamp_status = check_server_lamp(session)
                     server_lamp_on = server_lamp_status[LAMP_ON_KEY]
                     self.set_color(server_lamp_status[COLOR_KEY])
                     print('server says: ' + str(server_lamp_on))
@@ -110,14 +133,16 @@ class Lamp:
                             turn_off_lamp(self._pixels)
                     
                     time.sleep(POLLING_DELAY)
-                    self._checking_server = False
                 except Exception as e:
                     time.sleep(POLLING_DELAY)
-                    self._checking_server = False
                     print('error getting server lamp status')
                     print(e)
 
     def run(self):
+        """
+        Runs the lamp to manage the light on/off and color. Also responds user
+        interactions with the lamp for turning it on.
+        """
         while True:
             try:
                 if GPIO.input(21) == True:
@@ -125,17 +150,15 @@ class Lamp:
                         send_lamp_off()
                         self._lamp_on = False
                         turn_off_lamp(self._pixels)
-                        # self._check_server_after = datetime.datetime.now() + datetime.timedelta(hours = 1)
                     elif not self._lamp_on:
                         send_lamp_on()
                         self._lamp_on = True
                         turn_on_lamp(self._pixels, self._color)
-                        # self._check_server_after = datetime.datetime.now()
                     time.sleep(0.5)
             except Exception as e:
                 print('error on button press')
                 print(e)
 
 if __name__ == '__main__':
-    with Lamp() as lamp:
+    with FriendshipLamp() as lamp:
         lamp.run()
